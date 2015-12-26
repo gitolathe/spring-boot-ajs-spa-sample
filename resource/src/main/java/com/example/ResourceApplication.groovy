2aa4715c73a935bce6e119f571124e32ff7616e8
Ola Theander <ola.theander@myola.se>
2015-12-26 22:56:09 +0100

    Fixed files missed in the previous commit for part-IV.

--- resource/src/main/java/com/example/ResourceApplication.groovy
+++ resource/src/main/java/com/example/ResourceApplication.groovy
@@ -3,6 +3,8 @@ package com.example
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession
@@ -11,13 +13,19 @@ import org.springframework.session.web.http.HeaderHttpSessionStrategy
 @SpringBootApplication
 @RestController
 @EnableRedisHttpSession
-class ResourceApplication {
+class ResourceApplication extends WebSecurityConfigurerAdapter {
 
     @RequestMapping('/')
     def home() {
         [id: UUID.randomUUID().toString(), content: 'Hello World']
     }
     
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic().disable()
+        http.authorizeRequests().anyRequest().authenticated()
+    }
+  
     @Bean
     HeaderHttpSessionStrategy sessionStrategy() {
         new HeaderHttpSessionStrategy();
