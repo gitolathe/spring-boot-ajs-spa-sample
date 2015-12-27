3e83f690c942a86992cd30ba0cbed6020dcb27c2
Ola Theander <ola.theander@myola.se>
2015-12-27 19:35:24 +0100

    Implementing part-V - Oauth
    - copied authserver from Spring original
    - copied ResourceApplication.groovy & application.yml from Spring
      original.

--- resource/src/main/java/com/example/ResourceApplication.groovy
+++ resource/src/main/java/com/example/ResourceApplication.groovy
@@ -2,23 +2,21 @@ package com.example
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 @RestController
 @EnableResourceServer
-class ResourceApplication extends WebSecurityConfigurerAdapter {
+class ResourceApplication {
 
-  @RequestMapping('/')
-  def home() {
-    [id: UUID.randomUUID().toString(), content: 'Hello World']
-  }
+    @RequestMapping('/')
+    def home() {
+        [id: UUID.randomUUID().toString(), content: 'Hello World']
+    }
 
-  static void main(String[] args) {
-    SpringApplication.run ResourceApplication, args
-  }
+    static void main(String[] args) {
+        SpringApplication.run ResourceApplication, args
+    }
 }
